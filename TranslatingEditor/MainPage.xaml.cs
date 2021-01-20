bc95ac6e7ff212819e339a75903ab31476140ab3
YdrMaster <ydrml@hotmail.com>
2021-01-20 19:06:05 +0800

    添加项目文件。

---
+++ TranslatingEditor/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+using Windows.Storage.Pickers;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace TranslatingEditor {
+    public sealed partial class MainPage : Page {
+        public MainPage() => InitializeComponent();
+
+        private string text;
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            _ = ((Button)sender).Dispatcher.RunAsync(CoreDispatcherPriority.Normal, async () => {
+                var picker = new FileOpenPicker();
+                picker.FileTypeFilter.Add(".json");
+                var file = await picker.PickSingleFileAsync();
+                if (file == null)
+                    return;
+                TitleBlock.Text = file.Name;
+                _ = Windows.Storage.FileIO.ReadTextAsync(file).AsTask().ContinueWith(content => {
+                    text = content.Result;
+                    Debug.WriteLine($"Done. Read {text.Length} characters.");
+                });
+            });
+        }
+    }
+}
